201bc31cc2dcb96b3b9de37c7e67cf1e915e72a8
Nate Fonseka <nathal.fonseka@thomsonreuters.com>
2016-01-13 11:40:47 -0500

    done

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 var fs = require('fs');
-var json;
+var json, map;
 var express = require('express');
+var bodyParser = require("body-parser");
 var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 
 function readJsonFileSync(filepath, encoding){
 
@@ -13,14 +18,66 @@ function readJsonFileSync(filepath, encoding){
 }
 
 function getData(){
-    var filepath = __dirname + '/states.json';
+    var filepath = __dirname + '/statesA.json';
     return readJsonFileSync(filepath);
 }
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+function getMaxOfArray(numArray) {
+ return Math.max.apply(null, numArray);
+}
+
+function getMinOfArray(numArray) {
+ return Math.min.apply(null, numArray);
+}
+
+//init data
+function generateMap() {
+   json = getData();
+   map = {};
+   for(var i=0; i < json.length; i += 1) {
+       var stateObj = json[i];
+       var tempObj = {};
+       var borders = stateObj.border;
+       
+       var longitude = borders.map(function(elem) {
+          return elem[0]; 
+       });
+       
+       var latitude = borders.map(function(elem) {
+           return elem[1];
+       });
+       
+       tempObj.maxLong = getMaxOfArray(longitude);
+       tempObj.minLong = getMinOfArray(longitude);
+       
+       
+       tempObj.maxLat = getMaxOfArray(latitude);
+       tempObj.minLat = getMinOfArray(latitude);
+       
+       map[stateObj.state] = tempObj;
+   }
+}
+
+generateMap();
+
+function findState(long, lat) {
+    for(state in map) {
+        if(map[state].maxLong >= long && map[state].minLong <= long) {
+            if(map[state].maxLat >= lat && map[state].minLat <=lat) {
+                return state;
+            }
+        }
+    }
+    
+    return 'No State Found';
+}
+app.post('/', function (req, res) {
+  var long = parseFloat(req.body.longitude);
+  var lat  = parseFloat(req.body.latitude);
+  var state = findState(long,lat);
+  res.send(state);
 });
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port 8080!');
 })
